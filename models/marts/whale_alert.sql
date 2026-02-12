b67e814796774e814ed6b49db0da5a30ba8c0fe9
Sreekanth Ugrareddy <sreekanth.ugrareddy@wellpoint.com>
2026-02-12 15:31:25 -0500

    BTC models

---
+++ models/marts/whale_alert.sql
@@ -0,0 +1,21 @@
+WITH WHALES AS (
+
+SELECT
+output_address,
+sum(output_value) as total_sent,
+count(*) as tx_count
+FROM {{ ref('stg_btc_transactions') }}
+WHERE output_value > 10
+GROUP BY output_address
+ORDER BY total_sent DESC
+)
+
+)
+SELECT
+w.output_address,
+w.total_sent,
+w.tx_count,
+{{ convert_to_usd('w.total_sent') }} AS total_sent_usd
+FROM WHALES w
+
+order by total_sent_usd DESC
